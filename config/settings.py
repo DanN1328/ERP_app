4b1f99b18ea4197f5265db51159cac6c66d1f372
Edgar Gutierrez <edgardan200128@gmail.com>
2026-06-12 19:01:33 -0600

    cambios nuevos

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -137,3 +138,5 @@ REST_FRAMEWORK = {
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
